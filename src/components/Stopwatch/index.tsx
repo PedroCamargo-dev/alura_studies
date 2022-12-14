2c4e714164bd40a32366dfc0d79b17aab4709074
Pedro Camargo <pedrocamargo3008@gmail.com>
2022-12-14 17:07:09 -0300

    First commit

---
+++ src/components/Stopwatch/index.tsx
@@ -0,0 +1,15 @@
+import Button from "../Button";
+import Watch from "./Watch";
+import style from "./Stopwatch.module.scss"
+
+export function Stopwatch() {
+    return(
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o Cronometro</p>
+            <div className={style.relogioWrapper}>
+                <Watch />    
+            </div>
+            <Button texto="Começar!" />
+        </div>
+    )
+}
